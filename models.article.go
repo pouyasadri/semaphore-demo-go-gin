13c0f23e00b3f3067a33c6bba0699953981413a2
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-21 01:14:13 +0200

    adding getArticleByID function to models.article.go to fetch the article by its id form the article list

--- models.article.go
+++ models.article.go
@@ -1,11 +1,16 @@
 package main
 
+import "errors"
+
 type article struct {
 	ID      int    `json:"id"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
 
+// For this demo, we're storing the article list in memory
+// In a real application, this list will most likely be fetched
+// from a database or from static files
 var articleList = []article{
 	{ID: 1, Title: "Article 1", Content: "Article 1 body"},
 	{ID: 2, Title: "Article 2", Content: "Article 2 body"},
@@ -13,4 +18,14 @@ var articleList = []article{
 
 func getAllArticles() []article {
 	return articleList
+}
+
+func getArticleByID(id int) (*article, error) {
+	for _, a := range articleList {
+		if a.ID == id {
+			return &a, nil
+		}
+	}
+
+	return nil, errors.New("Article Not Found")
 }
