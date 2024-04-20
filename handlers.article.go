65d0bd8a19e6991a8f3327bfb7b0e0aa4d9fa884
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-21 01:22:05 +0200

    creating getArticle in handlers.article.go this function handles the  route and it response with a single articel based on the recieved id from the url params

--- handlers.article.go
+++ handlers.article.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,4 +14,24 @@ func showIndexPage(c *gin.Context) {
 		"title":   "Home Page",
 		"payload": articles,
 	})
+}
+
+func getArticle(c *gin.Context) {
+	//Check if the article ID is valid
+	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+		// Check if the article exists
+		if article, err := getArticleByID(articleID); err == nil {
+			// Call the HTML method of the context to render a template
+			c.HTML(http.StatusOK, "article.html", gin.H{
+				"title":   article.Title,
+				"payload": article,
+			})
+		} else {
+			// if the article is not found, abort with an error
+			c.AbortWithError(http.StatusNotFound, err)
+		}
+	} else {
+		// if an invalid article ID is specified in the URL, abort with an error
+		c.AbortWithStatus(http.StatusNotFound)
+	}
 }
