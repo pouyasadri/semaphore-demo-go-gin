4cb55e9da5bf3833d13e6f30cf2db76d7e2c813f
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-24 01:07:29 +0200

    add more tests due to adding database functionality to this program.
    Signed-off-by: pouyasadri <pouyapaghandeh78@gmail.com>

--- handlers.article_test.go
+++ handlers.article_test.go
@@ -8,15 +8,36 @@ import (
 	"testing"
 )
 
+func TestConnection(t *testing.T) {
+	err := DatabaseConnection()
+	// add some fake data to the database
+
+	if err != nil {
+		t.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	if DB == nil {
+		t.Fatal("DB is nil")
+	}
+
+	if err != nil {
+		t.Fatalf("Failed to insert article: %v", err)
+	}
+}
+
 // Test that a GET request to the home page returns the home page with
 // the HTTP code 200 for an unauthenticated user
 func TestShowIndexPageUnauthenticated(t *testing.T) {
 	r := getRouter(true)
 
-	r.GET("/", showIndexPage)
+	// Ensure the router has the same setup for /articles as the main application
+	articleRoutes := r.Group("/articles")
+	{
+		articleRoutes.GET("/", getArticles)
+	}
 
 	// Create a request to send to the above route
-	req, _ := http.NewRequest("GET", "/", nil)
+	req, _ := http.NewRequest("GET", "/articles/", nil)
 
 	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
 		// Test that the http status code is 200
@@ -24,7 +45,7 @@ func TestShowIndexPageUnauthenticated(t *testing.T) {
 
 		// Test that the page title is "Home Page"
 		p, err := io.ReadAll(w.Body)
-		pageOK := err == nil && strings.Index(string(p), "<title>Home Page</title>") > 0
+		pageOK := err == nil && strings.Contains(string(p), "<title>Home Page</title>") && strings.Contains(string(p), "<h2>Test Article</h2>")
 
 		return statusOK && pageOK
 	})
@@ -33,14 +54,14 @@ func TestShowIndexPageUnauthenticated(t *testing.T) {
 func TestArticleUnauthenticated(t *testing.T) {
 	r := getRouter(true)
 
-	r.GET("/article/view/:article_id", getArticle)
+	r.GET("/articles/:article_id", getArticle)
 
-	req, _ := http.NewRequest("GET", "/article/view/1", nil)
+	req, _ := http.NewRequest("GET", "/articles/1", nil)
 	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
 		statusOK := w.Code == http.StatusOK
 
 		p, err := io.ReadAll(w.Body)
-		pageOK := err == nil && strings.Index(string(p), "<title>Article 1</title>") > 0
+		pageOK := err == nil && strings.Index(string(p), "<title>Test Article</title>") > 0
 
 		return statusOK && pageOK
 	})
@@ -49,9 +70,11 @@ func TestArticleUnauthenticated(t *testing.T) {
 func TestArticleListJSON(t *testing.T) {
 	r := getRouter(false)
 
-	r.GET("/", showIndexPage)
-
-	req, _ := http.NewRequest("GET", "/", nil)
+	articleRoutes := r.Group("/articles")
+	{
+		articleRoutes.GET("/", getArticles)
+	}
+	req, _ := http.NewRequest("GET", "/articles/", nil)
 	req.Header.Add("Accept", "application/json")
 
 	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
@@ -64,9 +87,12 @@ func TestArticleListJSON(t *testing.T) {
 func TestArticleListXML(t *testing.T) {
 	r := getRouter(false)
 
-	r.GET("/", showIndexPage)
+	articleRoutes := r.Group("/articles")
+	{
+		articleRoutes.GET("/", getArticles)
+	}
 
-	req, _ := http.NewRequest("GET", "/", nil)
+	req, _ := http.NewRequest("GET", "/articles/", nil)
 	req.Header.Add("Accept", "application/xml")
 
 	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
@@ -79,9 +105,9 @@ func TestArticleListXML(t *testing.T) {
 func TestArticleUnauthenticatedJSON(t *testing.T) {
 	r := getRouter(true)
 
-	r.GET("/article/view/:article_id", getArticle)
+	r.GET("/articles/:article_id", getArticle)
 
-	req, _ := http.NewRequest("GET", "/article/view/1", nil)
+	req, _ := http.NewRequest("GET", "/articles/1", nil)
 	req.Header.Add("Accept", "application/json")
 
 	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
@@ -94,9 +120,9 @@ func TestArticleUnauthenticatedJSON(t *testing.T) {
 func TestArticleUnauthenticatedXML(t *testing.T) {
 	r := getRouter(false)
 
-	r.GET("/article/view/:article_id", getArticle)
+	r.GET("/articles/:article_id", getArticle)
 
-	req, _ := http.NewRequest("GET", "/article/view/1", nil)
+	req, _ := http.NewRequest("GET", "/articles/1", nil)
 	req.Header.Add("Accept", "application/xml")
 
 	testHTTPResponse(t, r, req, func(w *httptest.ResponseRecorder) bool {
