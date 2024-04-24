ea98caf783bb9f4fc1aeb62e227a5fdf6d55602d
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-24 13:54:03 +0200

    adding a route for render article creating html page.
    Signed-off-by: pouyasadri <pouyapaghandeh78@gmail.com>

--- main.go
+++ main.go
@@ -8,25 +8,34 @@ func main() {
 
 	// Set the router as the default one provided by Gin
 	router = gin.Default()
+
+	// Load the templates defined in the templates folder
 	router.LoadHTMLGlob("templates/*")
+	router.RedirectTrailingSlash = true
+
+	// Initialize database connection
 	err := DatabaseConnection()
 	if err != nil {
 		return
 	}
-	router.RedirectTrailingSlash = true
+
 	//redirect "/articles" to "/"
 	router.GET("/", func(c *gin.Context) {
 		c.Redirect(301, "/articles")
 
 	})
+
+	// Define the route for the index page and display the index.html template
 	articleRoutes := router.Group("/articles")
 	{
 		articleRoutes.GET("/", getArticles)
 		articleRoutes.GET("/:id", getArticle)
+		articleRoutes.GET("/new", showCreateArticlePage)
 		articleRoutes.POST("/", addArticle)
 		articleRoutes.PUT("/:id", updateArticle)
 		articleRoutes.DELETE("/:id", deleteArticle)
 	}
+
 	// Start serving the application
 	err = router.Run()
 	if err != nil {
