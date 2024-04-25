5a8e640b4754b05ef6cac7b15b5476efbd1ba7de
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-25 23:19:33 +0200

    adding `showEditArticlePage` to grab the article id from the url and get the article data from the `articles` table and sends data to `edit-article.html`.
    Signed-off-by: pouyasadri <pouyapaghandeh78@gmail.com>

--- handlers.article.go
+++ handlers.article.go
@@ -10,7 +10,10 @@ import (
 func getArticles(c *gin.Context) {
 	articles, err := getAllArticles()
 	if err != nil {
-		c.AbortWithError(http.StatusBadRequest, err)
+		err = c.AbortWithError(http.StatusBadRequest, err)
+		if err != nil {
+			return
+		}
 	}
 	render(c, gin.H{
 		"title":   "Home Page",
@@ -25,6 +28,24 @@ func showCreateArticlePage(c *gin.Context) {
 
 }
 
+func showEditArticlePage(c *gin.Context) {
+	if articleID, err := strconv.Atoi(c.Param("id")); err == nil {
+		if article, err := getArticleByID(articleID); err == nil {
+			render(c, gin.H{
+				"title":   "Edit Article",
+				"payload": article,
+			}, "edit-article.html")
+		} else {
+			err = c.AbortWithError(http.StatusNotFound, err)
+			if err != nil {
+				return
+			}
+		}
+	} else {
+		c.AbortWithStatus(http.StatusNotFound)
+	}
+}
+
 func getArticle(c *gin.Context) {
 	//Check if the article ID is valid
 	if articleID, err := strconv.Atoi(c.Param("id")); err == nil {
@@ -37,7 +58,10 @@ func getArticle(c *gin.Context) {
 			}, "article.html")
 		} else {
 			// if the article is not found, abort with an error
-			c.AbortWithError(http.StatusNotFound, err)
+			err = c.AbortWithError(http.StatusNotFound, err)
+			if err != nil {
+				return
+			}
 		}
 	} else {
 		// if an invalid article ID is specified in the URL, abort with an error
@@ -56,7 +80,7 @@ func addArticle(c *gin.Context) {
 		if err != nil {
 			c.AbortWithStatus(http.StatusInternalServerError)
 		} else {
-			c.JSON(http.StatusCreated, newArticle)
+			c.JSON(http.StatusCreated, gin.H{"message": "Article created successfully"})
 		}
 	} else {
 		// Get the input from the user
@@ -65,7 +89,10 @@ func addArticle(c *gin.Context) {
 
 		// Validate input
 		if title == "" || content == "" {
-			c.AbortWithError(http.StatusBadRequest, nil)
+			err := c.AbortWithError(http.StatusBadRequest, nil)
+			if err != nil {
+				return
+			}
 			return
 		}
 
@@ -81,7 +108,7 @@ func addArticle(c *gin.Context) {
 }
 
 func updateArticle(c *gin.Context) {
-	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+	if articleID, err := strconv.Atoi(c.Param("id")); err == nil {
 		if c.Request.Header.Get("Accept") == "application/json" {
 			var newArticle Article
 			if err := c.BindJSON(&newArticle); err != nil {
@@ -91,42 +118,22 @@ func updateArticle(c *gin.Context) {
 			if _, err := updateArticleByID(newArticle, articleID); err != nil {
 				c.AbortWithStatus(http.StatusInternalServerError)
 			} else {
-				c.JSON(http.StatusOK, newArticle)
-			}
-		} else {
-			title := c.PostForm("title")
-			content := c.PostForm("content")
-
-			// Validate input
-			if title == "" || content == "" {
-				c.AbortWithError(http.StatusBadRequest, nil)
-				return
-			}
-
-			// Update data in the database
-			newArticle := Article{ID: articleID, Title: title, Content: content}
-			if _, err := updateArticleByID(newArticle, articleID); err != nil {
-				c.AbortWithStatus(http.StatusInternalServerError)
-			} else {
-				c.Redirect(http.StatusMovedPermanently, "/")
+				c.JSON(http.StatusOK, gin.H{"message": "Article updated successfully"})
 			}
 		}
 	}
 }
 
 func deleteArticle(c *gin.Context) {
-	if articleID, err := strconv.Atoi(c.Param("article_id")); err == nil {
+	if articleID, err := strconv.Atoi(c.Param("id")); err == nil {
 		if c.Request.Header.Get("Accept") == "application/json" {
 			if _, err := deleteArticleByID(articleID); err != nil {
-				c.AbortWithError(http.StatusInternalServerError, err)
+				err := c.AbortWithError(http.StatusInternalServerError, err)
+				if err != nil {
+					panic(err)
+				}
 			} else {
-				c.JSON(http.StatusOK, gin.H{"message": "Article deleted"})
-			}
-		} else {
-			if _, err := deleteArticleByID(articleID); err != nil {
-				c.AbortWithError(http.StatusInternalServerError, err)
-			} else {
-				c.Redirect(http.StatusMovedPermanently, "/")
+				c.JSON(http.StatusOK, gin.H{"message": "Article deleted successfully"})
 			}
 		}
 	}
@@ -141,7 +148,7 @@ func render(c *gin.Context, data gin.H, templateName string) {
 		//Respond with JSON
 		c.JSON(http.StatusOK, data["payload"])
 	case "application/xml":
-		//Resond wiht XML
+		//Respond with XML
 		c.XML(http.StatusOK, data["payload"])
 	default:
 		c.HTML(http.StatusOK, templateName, data)
